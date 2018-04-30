468e93fcda7c21e9feeca54066c4627ba3a5162d
chester777 <aster9797@gmail.com>
2018-04-30 10:34:19 +0900

    week_000 is finished

--- chester777/week_000/main.cpp
+++ chester777/week_000/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include "main.h"
+
+/* class for problem solver
+ * */
+class solver {
+private:
+public:
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    FILE *fp; // must define for using freopen_s
+    freopen_s(&fp, "input.txt", "r", stdin);
+
+    int a;
+
+    scanf_s("%d", &a, sizeof(a));
+    printf("%d", a);
+
     return 0;
 }
---
+++ chester777/week_000/main.h
@@ -0,0 +1,13 @@
+//
+// Created by chester777 on 2018. 4. 16..
+//
+
+#ifndef WEEK_000_MAIN_H
+#define WEEK_000_MAIN_H
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+#endif //WEEK_000_MAIN_H
